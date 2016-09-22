9892dc4cac9a892abbdd976830b066ae99ee3459
Yuriy Musiychuk <ymusiychuk@lohika.com>
2016-09-22 17:44:16 +0300

    Added tests

---
+++ HelloWorld.Tests/HelloWorld.Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AdditionIsCorrect()
+        {
+            int a = 2;
+            int b = 5;
+
+            HelloWorld.Shared.HelloWorld hw = new Shared.HelloWorld();
+
+            Assert.IsTrue(hw.AddNumbers(a, b) == 7);
+        }
+    }
+}
